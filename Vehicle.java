bb9d5c9529df125d635326e0e02d238ca9af2228
Aditya Patil <adityapatil1998@gmail.com>
2020-01-30 16:48:40 +0530

    first commit

---
+++ Vehicle.java
@@ -0,0 +1,15 @@
+
+public interface Vehicle {
+	
+	public   int speed = 100;
+	public  String name = "Car";
+		
+	static void display() {
+		
+		System.out.println(name);
+		System.out.println(speed);
+		
+	}
+	
+	
+}
